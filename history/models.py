45285e267d431bda7abcf83976f20fc0de3681f2
amazoradi <amazoradi@gmail.com>
2019-01-22 20:23:13 -0600

    changed db to many to many for songs and artists, refactored

--- history/models.py
+++ history/models.py
@@ -7,6 +7,10 @@ class Artist(models.Model):
 
 
 class Song(models.Model):
-  artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+  # artist = models.ForeignKey(Artist, on_delete=models.CASCADE) 
   song_name = models.CharField(max_length=200)
   album_name = models.CharField(max_length=200)
+
+class Song_Artist(models.Model):
+  song =  models.ForeignKey(Song, on_delete=models.CASCADE)
+  artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
--- history/views.py
+++ history/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Artist
 from .models import Song
+from .models import Song_Artist
 from django.template import loader
 from django.shortcuts import get_object_or_404
 # Create your views here.
@@ -20,7 +21,13 @@ def artists(request):
 
 def details(request, artist_id):
   # return HttpResponse(f"Hello, world. You're at the artist {artist_id} detail page.") 
-  song_list = get_object_or_404(Artist, pk=artist_id)
-  context = {'song_list': song_list}
+  song_artist_list = get_object_or_404(Artist, id=artist_id)
+  # song_list = (Artist, song_artist_list.artist_id)
+  artist_songs = Song_Artist.objects.filter(artist_id = artist_id)
+  song_list = [Song.objects.filter(id=song.song_id) for song in artist_songs]
+  context = {'song_artist_list': song_artist_list,
+              'song_list': song_list}  
+  print(context)
+  print("songlist:", song_list[0])
   return render(request, 'history/details.html', context)
 
